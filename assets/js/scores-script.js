42aa557ad3be1067a46dca3196ef32a1f5c8bbbd
kpehl <30782087+kpehl@users.noreply.github.com>
2020-08-26 14:18:22 -0400

    Working on displaying high scores on high score page.  Loading in localStorage, creating list items, values undefined when put into list.  Debug. Changed list to ordered list instead of unordered.

--- assets/js/scores-script.js
+++ assets/js/scores-script.js
@@ -1,51 +1,45 @@
-// For the high score page, set up an event listener to clear the local storage when requested by button
+
+// Define an element to reference the score list
+scoreListEl = document.getElementById("score-list");
+
+
+// Load the high scores from local storage and display them in the list set up on the html page
+var loadScores = function() {
+    // retrieve the current high score list
+    var scores = localStorage.getItem("scores");
+
+    // if the localStorage does not have any values, set array to empty, otherwise parse into object
+    if (!scores) {
+        var scores = [];
+        console.log("no scores saved")
+    }
+    else {
+        scores = JSON.parse(scores);
+        console.log(scores)
+    }
+
+    console.log(scores.length)
+
+    // Create a list item and append it to the list for each score
+    for (var i = 0; i < scores.length; i++) {
+        console.log(scores.score)
+        console.log(scores.initials)
+        // create the list element
+        var scoreLi = document.createElement("li");
+        // set the content to be Initials: Score
+        scoreLi.textContent = scores.initials + ": " + scores.score;
+        // append the score to the list on the page
+        scoreListEl.appendChild(scoreLi);
+    }
+}
+
+
+
+
+// Set up an event listener to clear the local storage when requested by button
 
 var clearScoresEl = document.querySelector("#clear-data");
 
 clearScoresEl.addEventListener("click", function() {localStorage.clear()});
 
-// Function to create a score item using the user's input and appending that item to the high score list
-var createScoreEl = function(scoreDataObj) {
-    // Create the list item
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "score-item";
-
-    // Add a unique task id as a custom attribute
-    listItemEl.setAttribute("data-score-id", scoreIdCounter);
-
-    // Create a div to hold score info and add to list item
-    var scoreInfoEl = document.createElement("div");
-    // and give it a class name
-    scoreInfoEl.className = "score-content";
-
-    // Add HTML content to the div
-    scoreInfoEl.innerHTML = "<p class='score-item'>" + scoreDataObj.initials + ": " +scoreDataObj.score;
-    listItemEl.appendChild(scoreInfoEl);
-
-    // Add the unique task id to the taskDataObj for this task
-    scoreDataObj.id = scoreIdCounter;
-    //and add the taskDataObj to the tasks array - this will allow for data persistence
-    score.push(scoreDataObj);
-
-    // Save the data to local storage
-    saveScore();
-}
-
-// Load the high scores from local storage
-var loadScores = function() {
-    // Get task items from local storage
-    var savedScores = localStorage.getItem("scores");
-    // If the scores item does not exist, it will be null, and the function will exit without loading anything
-    if (!savedScores) {
-        return false;
-    }
-
-    // Convert tasks from the stringified format back into the array of objects
-    savedScores = JSON.parse(savedScores);
-
-    // Use the createScoreEl function to re-create the stored scores
-    for (var i = 0; i < savedScores.length; i++) {
-        createScoreEl(savedScores[i]);
-    }
-   
-}
+loadScores();
